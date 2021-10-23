6ae413e032a62b63fa7481d31b9b4e9576ceed2b
Fouad Karam <11132011+Fouad-Karam@users.noreply.github.com>
2021-10-23 00:45:20 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,24 @@ from tkinter import *
 from tkinter import messagebox
 from random import randint, choice, shuffle
 import pyperclip
+import json
+
+
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+    website = website_label_input.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(titl="Error", message="No data file found.")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title="Result", message=f"Website: {website}\nEmail: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"The website {website} does not exist in the database.")
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -32,19 +50,33 @@ def generate_password():
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 def save_data():
-    with open("data.txt", "a") as file:
-        website = website_label_input.get()
-        user_email = website_user_input.get()
-        password = website_password_input.get()
+    website = website_label_input.get()
+    user_email = website_user_input.get()
+    password = website_password_input.get()
+    new_data = {website:
+        {
+            "email": user_email,
+            "password": password,
+        }
+    }
 
-        if len(website) == 0 or len(password) == 0:
-            is_empty = messagebox.showinfo(title="Warning", message="You can not have empty fields!")
-        else:
-            is_ok = messagebox.askokcancel(title=website, message=f"Details entered are:\n Email: {user_email}\n "
-                                                                  f"Password: {password}\n Is this ok to save?")
-
-            if is_ok:
-                file.write(f"{website} | {user_email} | {password}\n")
+    if len(website) == 0 or len(password) == 0:
+        is_empty = messagebox.showinfo(title="Warning", message="You can not have empty fields!")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"Details entered are:\n Email: {user_email}\n "
+                                                              f"Password: {password}\n Is this ok to save?")
+        if is_ok:
+            try:
+                with open("data.json", "r") as data_file:
+                    data = json.load(data_file)  # Reading old data
+            except FileNotFoundError:
+                with open("data.json", "w") as data_file:
+                    json.dump(new_data, data_file, indent=4)
+            else:
+                data.update(new_data)  # Updating old data with new data
+                with open("data.json", "w") as data_file:
+                    json.dump(data, data_file, indent=4)  # Write new data to file
+            finally:
                 website_label_input.delete(0, END)
                 website_password_input.delete(0, END)
 
@@ -60,8 +92,8 @@ canvas.create_image(100, 100, image=lock_img)
 canvas.grid(column=1, row=0)
 
 # Input fields
-website_label_input = Entry(width=60)
-website_label_input.grid(column=1, row=1, columnspan=2)
+website_label_input = Entry(width=41)
+website_label_input.grid(column=1, row=1)
 website_label_input.focus()
 website_user_input = Entry(width=60)
 website_user_input.grid(column=1, row=2, columnspan=2)
@@ -78,6 +110,8 @@ website_password = Label(text="Password:")
 website_password.grid(column=0, row=3)
 
 # Button
+button_search = Button(text="Search", width=15, command=find_password)
+button_search.grid(column=2, row=1)
 button_generate = Button(text="Generate Password", command=generate_password)
 button_generate.grid(column=2, row=3)
 add_button = Button(text="Add", width=51, command=save_data)
